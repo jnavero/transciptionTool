5e6c40363eee899da83c748a98134d46bd9872b0
jnavero <jnavero@gmail.com>
2019-07-16 18:07:55 +0200

    Add Configurations

--- Configurations/Config.cs
+++ Configurations/Config.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,49 @@ namespace Configurations
 {
     public class Config
     {
-
         public bool ProyectSaved { get; set; }
 
-        public void SaveProyect()
+        public string TimesToRepeat { get; set; }
+        public string CutTime { get; set; }
+        public string Path { get; set; }
+        public string PauseBetTime { get; set; }
+        public string Position { get; set; }
+
+        public void SaveProyect(string projectFile)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrEmpty(projectFile))
+            {
+                string content = $"TimesToRepeat: {TimesToRepeat}\n";
+                content += $"PauseBetTime: {PauseBetTime}\n";
+                content += $"CutTime: {CutTime}\n";
+                content += $"Path: {Path}\n";
+                content += $"Position: {Position}\n";
+                File.WriteAllText(projectFile, content);
+            }
         }
+
+        public void LoadProject(string projectFile)
+        {
+            if (!string.IsNullOrEmpty(projectFile))
+            {
+                var content = File.ReadAllLines(projectFile);
+                foreach (var item in content)
+                {
+                    var variable = item.Split(new char[] { ':' });
+                    SetValue(variable[0], variable[1]);
+                }
+            }
+        }
+
+        private void SetValue(string name, string value)
+        {
+            var property = this.GetType().GetProperty(name);
+            if(property != null)
+            {
+                property.SetValue(this, value);
+            }
+        }
+
+
     }
 }
